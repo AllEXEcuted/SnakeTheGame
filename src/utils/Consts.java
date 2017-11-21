244d355ceaa158993d59e809eda38ba5227e2b90
AllEXEcuted <32677639+AllEXEcuted@users.noreply.github.com>
2017-11-22 02:43:57 +0500

    aaa

--- src/utils/Consts.java
+++ src/utils/Consts.java
@@ -1,16 +1,18 @@
 package utils;
 
 import java.util.HashMap;
+import java.util.List;
 
 import fieldObjects.AcceleratorBonus;
-import fieldObjects.FieldObject;
+import fieldObjects.FieldBonuses;
 import fieldObjects.ResetAcceleratorBonus;
 
 public class Consts {
-	public final HashMap<FieldObject, Boolean> bonuses;
-	
+	public final HashMap<FieldBonuses, Boolean> bonuses;
+	public static final String[] levels = new String[] {"Level1"};
+
 	public Consts(){
-		bonuses = new HashMap<FieldObject, Boolean>();
+		bonuses = new HashMap<FieldBonuses, Boolean>();
 		bonuses.put(new AcceleratorBonus(-1, -1), false);
 		bonuses.put(new ResetAcceleratorBonus(-1, -1), false);
 	}
